ba6a0049df7249e8f2a98143255ca00df7c5fc6c
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-05-03 15:37:03 +0200

    feat: upgrade to alphav4 of react-redux v7

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { decreaseCounter, increaseCounter, SET } from "./ducks/counter";
+import Count from "./Count";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -11,28 +12,43 @@ function useSquared() {
 
 function useReset() {
   const dispatch = useDispatch();
-  const toZero = () => dispatch({ type: SET, payload: 0 });
+  const toZero = useCallback(() => dispatch({ type: SET, payload: 0 }), [
+    dispatch
+  ]);
   return toZero;
 }
 
 function App() {
+  // set up a ticker
+  const [ticks, setTicks] = useState(0);
+  // and make it tick every second
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTicks(ticks + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
   // get the dispatch method from redux store using useDispatch
   const dispatch = useDispatch();
   // get the count using useSelector
   const count = useSelector(state => state.counter);
   // get the increase dispatcher from useRedux
-  const increase = () => dispatch(increaseCounter());
+  const increase = useCallback(() => dispatch(increaseCounter()), [dispatch]);
   // get the decrease dispatcher from useActions
-  const decrease = () => dispatch(decreaseCounter());
+  const decrease = useCallback(() => dispatch(decreaseCounter()), [dispatch]);
   // use a custom hook to get a pre-calculated value
   const squared = useSquared();
 
   // using the dispatch method, create an action
-  const setToRandom = () =>
-    dispatch({
-      type: SET,
-      payload: Math.floor(100 * Math.random())
-    });
+  const setToRandom = useCallback(
+    () =>
+      dispatch({
+        type: SET,
+        payload: Math.floor(100 * Math.random())
+      }),
+    [dispatch]
+  );
 
   // use Reset
   const reset = useReset();
@@ -40,19 +56,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>{ticks}</p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Count is: <code>{count}</code>
-        </p>
-        <p>
-          Squared count is: <code>{squared}</code>
-        </p>
-        <div>
-          <button onClick={increase}>INC</button>
-          <button onClick={decrease}>DEC</button>
-          <button onClick={setToRandom}>Random</button>
-          <button onClick={reset}>Reset</button>
-        </div>
+        <Count
+          count={count}
+          squared={squared}
+          increase={increase}
+          decrease={decrease}
+          setToRandom={setToRandom}
+          reset={reset}
+        />
       </header>
     </div>
   );
